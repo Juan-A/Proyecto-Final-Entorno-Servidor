2f4f9cd8f654c524ae4d42a25ed438d633c0fa37
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2024-01-23 15:45:08 +0100

    Added login button, added releated style. Modified css selectors.

--- inc/sections/nav_bar.php
+++ inc/sections/nav_bar.php
@@ -3,4 +3,8 @@
         <?= insert_logo() ?>
     </div>
     <? insertNavBarMenu(); ?>
+    <div >
+    <a href="login.php" class="loginNavButton"><i class='bx bx-user-circle bx-tada-hover' style="font-size: 30px;" ></i> Login</a>
+    
+    </div>
 </nav>
