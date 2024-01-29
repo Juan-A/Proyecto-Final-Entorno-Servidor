aef8854595e85e96812944bba50836986a40e8dd
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2024-01-29 19:13:32 +0100

    Created register page, main structure.

---
+++ register.php
@@ -0,0 +1,27 @@
+<?
+require_once("inc/modules/inc_global.php");
+require_once("inc/modules/inc_global_media.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- este título se obtiene desde la base de datos -->
+    <title><?= siteName() ?></title>
+    <link rel="stylesheet" href="inc/styles/main_style.css">
+    <link rel="stylesheet" href="inc/styles/auth_form_styles.css">
+    <script defer src="inc/modules/auth/samePassword.js"></script>
+</head>
+
+<body>
+    <!-- Adding navbar -->
+    <? require_once("inc/sections/nav_bar.php"); ?>
+    <!--Register box-->
+    <? require_once("inc/sections/register_form.php"); ?>
+    <!--Footer-->
+    <? require_once("inc/sections/footer.php"); ?>
+</body>
+
+</html>
