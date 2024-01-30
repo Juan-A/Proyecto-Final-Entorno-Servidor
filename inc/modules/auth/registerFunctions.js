2e1e38f74c05acc6b5736f4b57c0060ea22029e8
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2024-01-30 16:34:35 +0100

    Check user validity with js (at least 3 chars long) implemented.

--- inc/modules/auth/registerFunctions.js
+++ inc/modules/auth/registerFunctions.js
@@ -6,14 +6,37 @@ function verifySame(){
     }
     return true
 }
+function userIsValid(){
+    let user = document.querySelector("#username").value;
+    if(user.length <= 3 ){
+        return false
+    }
+    return true
 
-document.querySelector("#passwordConfirm").addEventListener("keyup",(evento, elemento) => {
+}
+
+document.querySelector("#passwordConfirm").addEventListener("keyup",(event, elemento) => {
     if(!verifySame()){
         document.querySelector("#regButton").disabled = true;
         document.querySelector(".errorMessage").hidden = false;
         document.querySelector(".errorMessage").innerHTML = "<i class='bx bx-error bx-flashing' ></i>"+"<span>Error, las contraseñas no coinciden.</span>";
 
     }else{
+        document.querySelector("#regButton").disabled = false;
+        document.querySelector(".errorMessage").hidden = true;
+        document.querySelector(".errorMessage").innerText = "";
+
+    }
+})
+document.querySelector("#username").addEventListener("keyup",(event, element) => {
+    if(!userIsValid()){
+        document.querySelector("#regButton").disabled = true;
+        document.querySelector(".errorMessage").hidden = false;
+        document.querySelector(".errorMessage").innerHTML = "<i class='bx bx-error bx-flashing' ></i>"+"<span>Error, el usuario es demasiado corto.</span>";
+
+    }else{
+        //User is taken will be implemented on this block later.
+        
         document.querySelector("#regButton").disabled = false;
         document.querySelector(".errorMessage").hidden = true;
         document.querySelector(".errorMessage").innerText = "";
