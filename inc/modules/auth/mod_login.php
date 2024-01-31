f0d6a875bb5067fc26dccd04593ddd68bf7eb622
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2024-01-31 17:25:54 +0100

    Added a funtion to check if user is logged in. Could be deleted.

--- inc/modules/auth/mod_login.php
+++ inc/modules/auth/mod_login.php
@@ -1,19 +1,20 @@
 <?php
 require_once("inc/modules/inc_global.php");
 
-function validateLogin($user,$password){
+function validateLogin($user, $password)
+{
     $query = "SELECT user_id,user_email,user_role,user_name,user_surname,user_password FROM db_users WHERE user_nickname = :user OR user_email = :user";
 
     $preQuery = db()->prepare($query);
-    $preQuery->bindParam(":user",$user);
+    $preQuery->bindParam(":user", $user);
 
-    if($preQuery->execute() && $preQuery->rowCount() >=1 ){
+    if ($preQuery->execute() && $preQuery->rowCount() >= 1) {
         $userData = $preQuery->fetch();
-        if(password_verify($password,$userData["user_password"])){
+        if (password_verify($password, $userData["user_password"])) {
             session_start();
-            $_SESSION["user_role"] =$userData["user_role"];
+            $_SESSION["user_role"] = $userData["user_role"];
             $_SESSION["user_name"] = $userData["user_name"];
-            $_SESSION["user_surname"] =$userData["user_surname"];
+            $_SESSION["user_surname"] = $userData["user_surname"];
             $_SESSION["user_email"] = $userData["user_email"];
             $_SESSION["logged_in"] = true;
             return true;
@@ -21,4 +22,13 @@ function validateLogin($user,$password){
     }
     return false;
 }
-?>
+function isLogged()
+{
+    if (isset($_SESSION["user_email"])) {
+        if ($_SESSION["logged_in"] == true) {
+            return true;
+        }
+        return false;
+    }
+    return false;
+}
