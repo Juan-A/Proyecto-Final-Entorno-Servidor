bc76a91728205a46bf50d625122007f38fdb8ab2
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2024-01-31 17:23:35 +0100

    Created logout. If logged show up the logout button instead of Login.

--- inc/sections/nav_bar.php
+++ inc/sections/nav_bar.php
@@ -4,6 +4,16 @@
     </div>
     <? insertNavBarMenu(); ?>
     <div>
-    <a href="login.php" class="loginNavButton"><i class='bx bx-user-circle bx-tada-hover' style="font-size: 30px;" ></i> Login</a>
+        <?
+        if (isLogged()) {
+        ?>
+            <a href="logout.php" class="loginNavButton"><i class='bx bx-exit bx-tada-hover' style="font-size: 30px;"></i> Logout</a>
+        <?
+        } else {
+        ?>
+            <a href="login.php" class="loginNavButton"><i class='bx bx-user-circle bx-tada-hover' style="font-size: 30px;"></i> Login</a>
+        <?
+        }
+        ?>
     </div>
 </nav>
