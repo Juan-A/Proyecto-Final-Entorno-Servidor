7ca37012ee4ec18cc530279ccac7b5642bf6c137
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2024-02-01 16:43:48 +0100

    Minor changes. Finished the function that checks if every field is filled, it could be used later.

--- inc/modules/auth/registerFunctions.js
+++ inc/modules/auth/registerFunctions.js
@@ -1,3 +1,5 @@
+const regButton = document.querySelector("#regButton")
+
 function verifySame(){
     let password = document.querySelector("#password").value
     let passwordConfirm = document.querySelector("#passwordConfirm").value
@@ -14,37 +16,48 @@ function userIsValid(){
     return true
 
 }
+
+//Could be useful if I want to check if all the fields are filled every second. Prevents user to...
 function filledAll(){
-    let password = document.querySelector("#password").value.length===0
-    let passwordConfirm = document.querySelector("#passwordConfirm").value.length===0
-    let user = document.querySelector("#username").value.length===0;
-    let name = document.querySelector("#name").value.length===0
+    let password = document.querySelector("#password").value.length!=0
+    let passwordConfirm = document.querySelector("#passwordConfirm").value.length!=0
+    let user = document.querySelector("#username").value.length!=0;
+    let name = document.querySelector("#name").value.length!=0
+    let surname = document.querySelector("#surname").value.length!=0
+    let mail = document.querySelector("#mail").value.length!=0
+
+    if(password && passwordConfirm && user && name && surname && mail ){
+        return false;
+    }
+    regButton.disabled=true;
+    console.log("meow")
+
 }
 
-document.querySelector("#regButton").disabled=true;
+regButton.disabled=true
+
 document.querySelector("#passwordConfirm").addEventListener("keyup",(event, elemento) => {
     if(!verifySame()){
-        document.querySelector("#regButton").disabled = true;
+        regButton.disabled = true;
         document.querySelector(".errorMessage").hidden = false;
         document.querySelector(".errorMessage").innerHTML = "<i class='bx bx-error bx-flashing' ></i>"+"<span>Error, las contraseñas no coinciden.</span>";
 
     }else{
-        document.querySelector("#regButton").disabled = false;
+        regButton.disabled = false;
         document.querySelector(".errorMessage").hidden = true;
         document.querySelector(".errorMessage").innerText = "";
-
     }
 })
 document.querySelector("#username").addEventListener("keyup",(event, element) => {
     if(!userIsValid()){
-        document.querySelector("#regButton").disabled = true;
+        regButton.disabled = true;
         document.querySelector(".errorMessage").hidden = false;
         document.querySelector(".errorMessage").innerHTML = "<i class='bx bx-error bx-flashing' ></i>"+"<span>Error, el usuario es demasiado corto.</span>";
 
     }else{
         //User is taken will be implemented on this block later.
         
-        document.querySelector("#regButton").disabled = false;
+        regButton.disabled = false;
         document.querySelector(".errorMessage").hidden = true;
         document.querySelector(".errorMessage").innerText = "";
 
