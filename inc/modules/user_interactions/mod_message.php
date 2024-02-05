6bb553ac458d5a4dd10b70661df5d61470d95c91
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2024-02-05 11:13:48 +0100

    Full implementation of message handler.

--- inc/modules/user_interactions/mod_message.php
+++ inc/modules/user_interactions/mod_message.php
@@ -1,25 +1,35 @@
 <?
-function handleMessage(){
-    
-    if(isset($_SESSION["message"])){
-        $messageArray=$_SESSION["message"];
+function handleMessage()
+{
+
+    if (isset($_SESSION["message"])) {
+        $messageArray = $_SESSION["message"];
+        $finalMessage = "<div id='message'>";
         foreach ($messageArray as $key => $message) {
-            if($message[1]==1){
-                echo "<div id='message'><span class='error'>".$message[0]."</span></div>";
-            }else if($message[1]==0){
-                echo "<div id='message'><span class='success'>".$message[0]."</span></div>";
+            if ($message[1] == 1) {
+                $finalMessage .= "<span class='error'>" . $message[0] . "</span>";
+            } else if ($message[1] == 0) {
+                $finalMessage .= "<span class='success'>" . $message[0] . "</span>";
             }
         }
-        
+        echo $finalMessage."</div>";
     }
     unset($_SESSION["message"]);
 }
-function addMessage($message,$status){
+function addMessage($message, $status)
+{
     /*Stores the error message in an array, [message,status]:
     Status codes:
     0 -> Success
     1-> Error
     */
-    $_SESSION["message"] =[];
-    array_push($_SESSION["message"],[$message,$status]);
+
+        if(isset($_SESSION["message"]) && is_array($_SESSION["message"])){
+            array_push($_SESSION["message"], [$message, $status]);
+        }else{
+            $_SESSION["message"] = [];
+            array_push($_SESSION["message"], [$message, $status]);
+        }
+    
+    
 }
