c11e56138aaeca20c9fcb7d06013d4f0daef8eee
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2024-02-05 11:15:12 +0100

    Required tag on all fields. Server Side verification will be implemented soon.

--- inc/sections/register_form.php
+++ inc/sections/register_form.php
@@ -5,17 +5,17 @@
                 <legend>Datos de acceso</legend>
                 <div class="authFields">
                     <span>Usuario / nick:</span>
-                    <input type="text" name="username" id="username">
+                    <input type="text" name="username" id="username" required>
                     <span>Nombre:</span>
-                    <input type="text" name="name" id="name">
+                    <input type="text" name="name" id="name" required>
                     <span>Apellidos:</span>
-                    <input type="text" name="surname" id="surname">
+                    <input type="text" name="surname" id="surname" required>
                     <span>Correo electronico:</span>
-                    <input type="text" name="mail" id="mail">
+                    <input type="text" name="mail" id="mail" required>
                     <span>Contraseña:</span>
-                    <input type="password" name="password" id="password">
+                    <input type="password" name="password" id="password" required>
                     <span>Confirmación de contraseña:</span>
-                    <input type="password" name="passwordConfirm" id="passwordConfirm">
+                    <input type="password" name="passwordConfirm" id="passwordConfirm" required>
                     <span class="errorMessage" hidden></span>
                     <button type="submit" id="regButton" class="formBtn">Registrarse</button>
                 </div>
