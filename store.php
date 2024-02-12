aec1f6ffe862b24caf9b57c4278e7046994e5b30
Juan Antonio Herrero Centurion <juan13herrero@gmail.com>
2024-02-12 18:29:44 +0100

    Created store page.

---
+++ store.php
@@ -0,0 +1,20 @@
+<?
+require_once("inc/modules/inc_global.php");
+require_once("inc/modules/inc_global_media.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- este título se obtiene desde la base de datos -->
+    <title><?=siteName()?></title>
+    <link rel="stylesheet" href="inc/styles/main_style.css">
+</head>
+<body>
+<!-- Adding navbar -->
+<? require_once("inc/sections/nav_bar.php"); ?>
+<? handleMessage() ?>
+<? require_once("inc/sections/footer.php"); ?>
+</body>
+</html>
