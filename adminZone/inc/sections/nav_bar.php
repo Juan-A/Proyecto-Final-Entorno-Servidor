f5e1ac8a837c392da180778ba54f182eb00cbcea
Juan Antonio Herrero Centurion <juan13herrero@gmail.com>
2024-02-13 17:15:14 +0100

    Implemented nav menu for the admin zone.

--- adminZone/inc/sections/nav_bar.php
+++ adminZone/inc/sections/nav_bar.php
@@ -2,16 +2,8 @@
 
     <? insertNavBarMenu(); ?>
     <div>
-        <?
-        if (isLogged()) {
-        ?>
-            <a href="logout.php" class="loginNavButton"><i class='bx bx-exit bx-tada-hover' style="font-size: 30px;"></i> Logout</a>
-        <?
-        } else {
-        ?>
-            <a href="login.php" class="loginNavButton"><i class='bx bx-user-circle bx-tada-hover' style="font-size: 30px;"></i> Login</a>
-        <?
-        }
-        ?>
+
+        <a href="../index.php" class="returnNavButton"><i class='bx bx-user-circle bx-tada-hover' style="font-size: 30px;"></i> Regresar al sitio</a>
+
     </div>
 </nav>
