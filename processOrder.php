92c5e6e19b73070258dc8c9b833ff19e87274678
Juan Antonio Herrero Centurion <juan13herrero@gmail.com>
2024-03-15 15:59:41 +0100

    Minor fixes.

--- processOrder.php
+++ processOrder.php
@@ -17,9 +17,17 @@ try {
     $orderId = createOrder($_SESSION["user_id"], $_POST["address"], $_POST["mobile"], $_SESSION["user_email"]);
     $mail = createMail($orderId, $_SESSION["user_email"], $_SESSION["user_name"] . " " . $_SESSION["user_surname"], $_POST["address"]);
     $confMail = loadMailConf($server, $port, $user, $password, $encType, $useEnc);
+    //Enviar correos
+
+    //Enviar correo al usuario
     sendMail($confMail, $_SESSION["user_email"], $_SESSION["user_name"] . " " . $_SESSION["user_surname"], $mail, "Pedido " . $orderId);
+    //Enviar correo al administrador
+    sendMail($confMail, $user, returnSiteName(), $mail, "Pedido Entrante - " . $orderId);
+    //Limpiar carrito
     unset($_SESSION["cart"]);
+    //Mensaje de éxito
     addMessage("¡Su pedido nº $orderId ha sido realizado correctamente!", 0);
+    //Redirigir a la tienda
     header("Location: store.php");
 } catch (Error $e) {
     addMessage("Hubo un error durante la creación del pedido.", 1);
